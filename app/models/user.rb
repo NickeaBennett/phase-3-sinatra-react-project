94f7e158504c0741f2f80edbc51d71c3daac62f4
NickeaBennett <nickeabennett@gmail.com>
2022-08-11 20:40:08 -0400

    Updated backend to comply with requirements

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  has_many :tasks
+
+  validates :name, presence: true, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,54 @@
+require "date"
+
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+
+User.create(name: "Nick")
+User.create(name: "Tom")
+User.create(name: "Thompson")
+User.create(name: "John")
+
+Task.create(
+    {name: "Meditate",
+    description: "Be one with self!",
+    user_id: rand(1..3)}
+)
+
+Task.create(
+    {name: "Eat food",
+    description: "Don't forget to eat!",
+    user_id: rand(1..3)}
+)
+
+Task.create(
+    {name: "Excercise",
+    description: "Get and stay in shape",
+    user_id: rand(1..3)}
+)
+
+Task.create(
+    {name: "Spend time with children",
+    description: "Take children to park and movies",
+    user_id: rand(1..3)}
+)
+
+Task.create(
+    {name: "Clean house",
+    description: "Vaccum and wash dishes",
+    user_id: rand(1..3)}
+)
+
+Task.create(
+    {name: "Study",
+    description: "Time management for studying",
+    user_id: rand(1..3)}
+)
+
+Task.create(
+    {name: "Job hunting",
+    description: "Update resume and apply for jobs",
+    user_id: rand(1..3)}
+)
+
 
 puts "✅ Done seeding!"
